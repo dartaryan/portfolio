0e9d03d79b1181ffe46e1362b459a155f701a2bd
Ben Akiva <dartaryan@gmail.com>
2021-09-25 17:05:50 +0300

    create testimonial

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,10 +73,26 @@ window.addEventListener("scroll", scrollTop);
 /*===== SHOW SCROLL TOP =====*/
 
 /*===== MIXITUP FILTER PORTFOLIO =====*/
-
-
+var mixer = mixitup(".portfolio__container", {
+  selectors: {
+    target: ".portfolio__content",
+  },
+  animation: {
+    duration: 400,
+  },
+});
 
 /* Link active portfolio */
+const linkPortfolio = document.querySelectorAll(".portfolio__item");
+
+function activePortfolio() {
+  if (linkPortfolio) {
+    linkPortfolio.forEach((l) => l.classList.remove("active-portfolio"));
+    this.classList.add("active-portfolio");
+  }
+}
+
+linkPortfolio.forEach((l) => l.addEventListener("click", activePortfolio));
 
 /*===== SWIPER CAROUSEL =====*/
 
